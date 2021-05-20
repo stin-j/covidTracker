145765bfb3eba4134c2d01cf634ffc2f685ffad7
stin-j <39718024+stin-j@users.noreply.github.com>
2021-05-19 20:16:25 -0700

    Update readData.java

--- covidTracker/readData.java
+++ covidTracker/readData.java
@@ -6,17 +6,24 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class readData {
 
 	public static void main(String[] args) throws IOException {
-		URL url = new URL("https://corona.lmao.ninja/");
+		JSONParser jsonParser = new JSONParser();
+		URL url = new URL("https://api.covidactnow.org/v2/states.json?apiKey=4b015525bf704af98038f30aab1088f6");
 		URLConnection connection = url.openConnection();
 		InputStream input = connection.getInputStream();
 		BufferedReader br = new BufferedReader(new InputStreamReader(input));
 		String line = null;
 		while ((line = br.readLine()) != null) {
+			
 			System.out.println(line);
 		}
 	}
 }
+//4b015525bf704af98038f30aab1088f6
