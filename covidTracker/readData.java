13b3ddf4b970cbc31b9d5f1c81c9703959e0d6d8
stin-j <39718024+stin-j@users.noreply.github.com>
2021-05-20 14:37:30 -0700

    Update readData.java
    streamlining with new method

--- covidTracker/readData.java
+++ covidTracker/readData.java
@@ -19,43 +19,54 @@ public class readData {
 
 	public static void main(String[] args) throws IOException {
 		try {
-            URL url = new URL("https://disease.sh/v3/covid-19/all");
-            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            URL all = new URL("https://disease.sh/v3/covid-19/all");
+            URL states = new URL("https://disease.sh/v3/covid-19/states");
+            URL continent = new URL("https://disease.sh/v3/covid-19/continents");
+            URL country = new URL ("https://disease.sh/v3/covid-19/countries");
+            
+            HttpURLConnection con = (HttpURLConnection) all.openConnection();
             con.setRequestMethod("GET");
             con.connect();
             int responsecode = con.getResponseCode();
             if (responsecode != 200) {
                 throw new RuntimeException("HttpResponseCode: " + responsecode);
-            } else {
-                String file = "";
-                Scanner scanner = new Scanner(url.openStream());
-                while (scanner.hasNext()) {
-                    file += scanner.next();
-                }
-                scanner.close();
-                JSONParser parse = new JSONParser();
-                JSONObject data_obj = (JSONObject) parse.parse(file);
-                //JSONObject obj =  (JSONObject) data_obj.get("cases");
-                System.out.println("Total Cases: "+data_obj.get("cases"));
-                System.out.println("New Cases Today: "+data_obj.get("todayCases"));
-                System.out.println("Total Deaths: "+data_obj.get("deaths"));
-                System.out.println("Deaths Today: "+data_obj.get("todayDeaths"));
-                System.out.println("Total Recovered: "+data_obj.get("cases"));
-                System.out.println("New Recoveries Today: "+data_obj.get("todayRecovered"));
-                System.out.println("Current Active Cases: "+data_obj.get("active"));
-                
+            } //I'm only using one site's API so the response code should be the same for all links
+            
+            else {
+                JSONObject data_all = dataInput(all);
+                readAll objAll = new readAll(data_all);
+                objAll.printTotals(); 
             }
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 	}
+	
+	public static JSONObject dataInput(URL url) throws IOException, ParseException { //everything inside the link to a JSON Object yay
+		JSONParser parse = new JSONParser();
+		String file = "";
+		Scanner scanner = new Scanner(url.openStream());
+        while (scanner.hasNext()) {
+            file += scanner.next();
+        }
+        scanner.close();
+        JSONObject data = (JSONObject) parse.parse(file);
+        return data;
+	}
 }
-//4b015525bf704af98038f30aab1088f6
+//COVIDACTNOW.ORG
+//4b015525bf704af98038f30aab1088f6 <--- API KEY
 //current data for all states: https://api.covidactnow.org/v2/states.json?apiKey=4b015525bf704af98038f30aab1088f6
 //historic data for all states: https://api.covidactnow.org/v2/states.timeseries.json?apiKey=4b015525bf704af98038f30aab1088f6
 //current data for California: https://api.covidactnow.org/v2/state/CA.json?apiKey=4b015525bf704af98038f30aab1088f6
 //historic data for California: https://api.covidactnow.org/v2/state/CA.timeseries.json?apiKey=4b015525bf704af98038f30aab1088f6
 //data for all counties in California: https://api.covidactnow.org/v2/county/CA.json?apiKey=4b015525bf704af98038f30aab1088f6
 //aggregated US data: https://api.covidactnow.org/v2/country/US.json?apiKey=4b015525bf704af98038f30aab1088f6
+
+//disease.sh (easier to use rn)
 //global summary: https://disease.sh/v3/covid-19/all
+//by state: https://disease.sh/v3/covid-19/states
+//California data (don't need tbh): https://disease.sh/v3/covid-19/states/California
+//by continent: https://disease.sh/v3/covid-19/continents
+//by country: https://disease.sh/v3/covid-19/countries
