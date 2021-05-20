98b219dd19cd31687c5f856844a0a34992c87ced
stin-j <39718024+stin-j@users.noreply.github.com>
2021-05-20 14:46:27 -0700

    Update dataAll.java

--- covidTracker/dataAll.java
+++ covidTracker/dataAll.java
@@ -1,15 +1,5 @@
 package covidTracker;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Scanner;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
